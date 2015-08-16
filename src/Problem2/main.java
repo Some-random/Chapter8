b88ec008e0841cabe9c4cfb693f4c7477248a8ae
Bob_JIANG <jdw19930725@gmail.com>
2015-08-16 21:34:14 +0800

    Finished Problem2, The minimal class structure to get things done. Notice the use of CallHelper and Arraylist

---
+++ src/Problem2/main.java
@@ -0,0 +1,15 @@
+package Problem2;
+
+/**
+ * Created by Bob_JIANG on 8/16/2015.
+ */
+public class main {
+    //The minimal class structure to get things done. Notice the use of CallHelper and Arraylist
+    public static void main(String[] args) {
+        CallHelper CH = new CallHelper();
+        CH.disPatchCall(6);
+        CH.disPatchCall(7);
+        CH.disPatchCall(8);
+        CH.disPatchCall(2);
+    }
+}
